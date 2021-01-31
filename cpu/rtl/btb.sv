4cd1c6217d3839e1aeb736f2be9600aa69f6f8c5
ide <ide@ny.ics.keio.ac.jp>
2021-01-31 16:38:19 +0900

    Updated

--- cpu/rtl/btb.sv
+++ cpu/rtl/btb.sv
@@ -14,167 +14,153 @@ module btb #(
 	parameter ADDR = `AddrWidth,
 	parameter INST = `InstWidth,
 	parameter BTB_D = `BtbTableDepth,
-	parameter FETCH = 1,
-	parameter SIMBRCOM = 1,
 	parameter CNT = `BtbCntWidth
 )(
 	input wire						clk,
 	input wire						reset_,
 
-	/* prediction */
-	input wire [FETCH*ADDR-1:0]		btb_addr,
-	output wire [FETCH-1:0]			target_valid,	// target is valid
-	output wire [FETCH*ADDR-1:0]	target_addr,	// target address
+	// prediction
+	input wire [ADDR-1:0]			pc,
+	output wire						btb_hit,		// target is valid
+	output wire [ADDR-1:0]			btb_addr,		// target address
 
-	/* train */
-	input wire [SIMBRCOM-1:0]		target_miss_,	// indirect jump target miss
-	input wire [SIMBRCOM-1:0]		pc_chg_com_,	// br/jump commit
-	input wire [SIMBRCOM-1:0]		chg_taken_,		// taken
-	input wire [SIMBRCOM*ADDR-1:0]	com_addr,		// commit pc
-	input wire [SIMBRCOM*ADDR-1:0]	com_tar_addr	// target address of commited inst
+	// train
+	input wire						br_commit_,		// branch commit
+	input wire 						br_taken_,		// branch taken
+	input wire						br_miss_,		// branch prediction miss
+	input wire 						jump_commit_,	// br/jump commit
+	input wire						jump_miss_,		// indirect jump target miss
+	input wire [ADDR-1:0]			com_addr,		// commit pc
+	input wire [ADDR-1:0]			com_tar_addr	// target address of commited inst
 );
 
-	/***** internal parameters *****/
+	//***** internal parameters
 	localparam INST_OFS = $clog2(INST/`ByteBitWidth);
 	localparam BTB_ADDR = $clog2(BTB_D);
 	localparam BTB_TAG = ADDR - BTB_ADDR - INST_OFS;
 	localparam CNT_MAX = {CNT{1'b1}};
 	localparam CNT_MIN = {CNT{1'b0}};
 	localparam CNT_DEF = CNT_MAX;
-	/***** register *****/
+
+	//***** internal register
 	reg [ADDR-1:0]					addr_buf [BTB_D-1:0];
 	reg [BTB_TAG-1:0]				tag [BTB_D-1:0];
 	reg [CNT-1:0]					cnt [BTB_D-1:0];
-	/***** internal wires *****/
-	/* for train */
-	wire [BTB_TAG*SIMBRCOM-1:0]		tr_tag;
-	wire [BTB_ADDR*SIMBRCOM-1:0]	tr_idx;
-	wire [ADDR-1:0]					next_addr_buf [BTB_D-1:0];
-	wire [BTB_TAG-1:0]				next_tag [BTB_D-1:0];
-	wire [CNT-1:0]					next_cnt [BTB_D-1:0];
+
+	//***** internal wires
+	//*** input pc
+	wire [BTB_ADDR-1:0]				btb_idx;
+	wire [BTB_TAG-1:0]				in_pc_tag;
+	//*** prediction
+	wire [BTB_TAG-1:0]				pred_tag;
+	wire [CNT-1:0]					pred_cnt;
+	wire							tag_match;
+	wire							entry_confident;
+	//*** training
+	wire [BTB_ADDR-1:0]				com_idx;
+	wire [BTB_TAG-1:0]				com_tag;
+	wire [CNT-1:0]					com_cnt;
+	wire							com_tag_match;
+	wire							com_confident;
+	//wire [BTB_TAG*SIMBRCOM-1:0]	tr_tag;
+	//wire [BTB_ADDR*SIMBRCOM-1:0]	tr_idx;
+	//wire [ADDR-1:0]				next_addr_buf [BTB_D-1:0];
+	//wire [BTB_TAG-1:0]			next_tag [BTB_D-1:0];
+	//wire [CNT-1:0]				next_cnt [BTB_D-1:0];
+
+	//***** combinational cells 
+	logic							buf_we_;
+	logic							cnt_we_;
+	logic [CNT-1:0]					next_cnt;
 
 
-	/***** prediction *****/
-	generate
-		genvar gi;
-		for ( gi = 0; gi < FETCH; gi = gi + 1 ) begin : Loop_pred
-			wire [BTB_ADDR-1:0]		pred_idx;
-			wire [ADDR-1:0]			addr_each;
-			wire [BTB_TAG-1:0]		tag_each;
-			wire					tag_match;
-			wire [CNT-1:0]			cnt_each;
-			wire					valid_tmp;
-			/* entry setup */
-			assign addr_each = btb_addr[`RangeG(gi,ADDR)];
-			assign pred_idx = addr_each[BTB_ADDR+INST_OFS-1:INST_OFS];
 
-			/* tag check */
-			assign tag_each = addr_each[ADDR-1:BTB_ADDR+INST_OFS];
-			assign tag_match = ( tag_each == tag[pred_idx] );
-
-			/* prediction */
-			assign cnt_each = cnt[pred_idx];
-			assign valid_tmp = cnt_each[CNT-1];
-			assign target_valid[gi] = tag_match & valid_tmp;
-			assign target_addr[`RangeG(gi,ADDR)] = addr_buf[pred_idx];
-		end
-	endgenerate
+	//***** output assign
+	assign target_addr = addr_buf[btb_idx];
+	assign btb_hit = tag_match && entry_confident;
 
 
-	/***** train *****/
-	/* generte index */
-	generate
-		genvar gj;
-		for ( gj = 0; gj < SIMBRCOM; gj = gj + 1 ) begin : Loop_com
-			wire [ADDR-1:0]			addr_each;
-			wire [BTB_TAG-1:0]		tag_each;
-			wire [BTB_ADDR-1:0]		idx_each;
-			/* entry setup */
-			assign addr_each = com_addr[`RangeG(gj,ADDR)];
-			assign idx_each = addr_each[BTB_ADDR+INST_OFS-1:INST_OFS];
-			assign tr_idx[`RangeG(gj,BTB_ADDR)] = idx_each;
 
-			/* tag check */
-			assign tag_each = addr_each[ADDR-1:BTB_ADDR+INST_OFS];
-			assign tr_tag[`RangeG(gj,BTB_TAG)] = tag_each;
-		end
-	endgenerate
+	//***** internal assign
+	//*** prediction
+	assign btb_idx = pc[BTB_ADDR+INST_OFS-1:INST_OFS];
+	assign in_pc_tag = pc[ADDR-1:BTB_ADDR+INST_OFS];
+	assign pred_tag = tag[btb_idx];
+	assign tag_match = ( in_pc_tag == pred_tag );
+	assign pred_cnt = cnt[btb_idx];
+	assign entry_confident = pred_cnt[CNT-1];
+	//*** table update
+	assign com_idx = com_addr[BTB_ADDR+INST_OFS-1:INST_OFS];
+	assign com_tag = com_addr[ADDR-1:BTB_ADDR+INST_OFS];
+	assign com_cnt = cnt[com_idx];
+	assign com_tag_match = ( com_tag == tag[com_idx] );
+	assign com_confident = com_cnt[CNT-1];
 
-	/* update data */
-	generate
-		genvar gk;
-		for ( gk = 0; gk < BTB_D; gk = gk + 1 ) begin : Loop_train
-			assign {next_addr_buf[gk], next_tag[gk], next_cnt[gk]}
-				= update_table(gk, pc_chg_com_, tr_idx, tr_tag, 
-					com_tar_addr, chg_taken_, addr_buf[gk], tag[gk], cnt[gk]);
-		end
-	endgenerate
 
-	/* update functions */
-	localparam UPDATE_TABLE = ADDR + BTB_TAG + CNT;
-	function [UPDATE_TABLE-1:0] update_table;
-		input [BTB_ADDR-1:0]			idx;			// index
-		input [SIMBRCOM-1:0]			commit_;		// br inst commited
-		input [BTB_ADDR*SIMBRCOM-1:0]	tr_idx;			// training data
-		input [BTB_TAG*SIMBRCOM-1:0]	tr_tag;			//
-		input [SIMBRCOM*ADDR-1:0]		tr_target;		//
-		input [SIMBRCOM-1:0]			tr_taken_;		//
-		input [ADDR-1:0]				cur_addr_buf;	// current data
-		input [BTB_TAG-1:0]				cur_tag;		//
-		input [CNT-1:0]					cur_cnt;		//
-		reg [SIMBRCOM-1:0]				idx_match;
-		reg [SIMBRCOM-1:0]				tag_match;
-		reg [BTB_TAG-1:0]				tr_tag_sep [SIMBRCOM-1:0];
-		reg [ADDR-1:0]					tr_target_sep [SIMBRCOM-1:0];
-		reg								confident;
-		reg [ADDR-1:0]					out_addr_buf;
-		reg [BTB_TAG-1:0]				out_tag;
-		reg [CNT-1:0]					out_cnt;
-		integer i;
-		begin
-			confident = cur_cnt[CNT-1];
-			out_addr_buf = cur_addr_buf;
-			out_tag = cur_tag;
-			out_cnt = cur_cnt;
-			/* tag matching */
-			for ( i = 0; i < SIMBRCOM; i = i + 1 ) begin
-				/* separate */
-				tr_tag_sep[i] = tr_tag[`RangeF(i,BTB_TAG)];
-				tr_target_sep[i] = tr_target[`RangeF(i,ADDR)];
-				/* matching */
-				idx_match[i] = ( tr_idx[`RangeF(i,BTB_ADDR)] == idx );
-				tag_match[i] = ( tr_tag[`RangeF(i,BTB_TAG)] == cur_tag );
-			end
 
-			/* select slot */
-			for ( i = 0; i < SIMBRCOM; i = i + 1 ) begin
-				if ( idx_match[i] && !commit_[i] ) begin
-					if ( tag_match[i] ) begin
-						/* for tag matched branch/jump */
-						if ( tr_taken_[i] == `Enable_ ) begin
-							out_cnt = `CntUp(out_cnt,CNT_MAX,1'b1);
-						end else begin
-							out_cnt = `CntDwn(out_cnt,CNT_MIN,1'b1);
-						end
-					end else begin
-						/* for other branch/jump */
-						if ( tr_taken_[i] == `Enable_ ) begin
-							out_cnt = confident ? out_cnt - 1 : CNT_DEF;
-							out_addr_buf 
-								= confident ? cur_addr_buf : tr_target_sep[i];
-							out_tag = confident ? cur_tag : tr_tag_sep[i];
-						end
+	//***** combinational logics
+	always_comb begin
+		//*** btb update
+		unique if ( br_commit_ == `Enable_ ) begin
+			if ( tag_match ) begin
+				// when a commited branch is tracked
+				buf_we_ = `Disable_;
+				cnt_we_ = `Enable_;
+				case ( br_taken_, br_miss_ )
+					{`Enable_, `Disable_} : begin
+						next_cnt = CNT_MAX:
+						cnt_we_ = `Enable_;
 					end
-				end
+					{`Enable_, `Enable_ } : begin
+						next_cnt = `CntUp(com_cnt,CNT_MAX,1'b1);
+						cnt_we_ = `Enable_;
+					end
+					default : begin
+						next_cnt = `CntDwn(com_cnt,CNT_MIN,1'b1);
+					end
+				endcase
+			end else begin
+				// when a commited branch is not tracked
+				case ( br_taken_, br_miss_ )
+					{`Enable_, `Disable_} : begin
+						next_cnt = CNT_MAX:
+						cnt_we_ = `Enable_;
+						buf_we_ = `Enable_;
+					end
+					{`Enable_, `Enable_ } : begin
+						// conservatively replace entry,
+						//		if old entry is unconfident
+						next_cnt = CNT_MIN;
+						cnt_we_ = com_confident;
+						buf_we_ = com_confidnet;
+					end
+					default : begin
+						next_cnt = {CNT{1'b0}};
+						cnt_we_ = `Disable_;
+						buf_we_ = `Disable_; 
+					end
+				endcase
 			end
-			update_table = {out_addr_buf, out_tag, out_cnt};
+		end else if ( jump_commit_ == `Enable_ ) begin
+			next_cnt = CNT_MAX;
+			cnt_we_ = `Enable_;
+			if ( jump_miss_ == `Enable_ ) begin
+				buf_we_ = `Enable_;
+			end else begin
+				buf_we_ = `Disable_;
+			end
+		end else begin
+			buf_we_ = `Disable_;
+			cnt_we_ = `Disable_;
+			next_cnt = {CNT{1'b0}};
 		end
-	endfunction
+	end
 
 
-	/***** sequential logics *****/
-	integer i;
+
+	//***** sequential logics
 	always_ff @( posedge clk or negedge reset_ ) begin
+		int i;
 		if ( reset_ == `Enable_ ) begin
 			for ( i = 0; i < BTB_D; i = i + 1 ) begin
 				addr_buf[i] <= {BTB_TAG{1'b0}};
@@ -182,10 +168,12 @@ module btb #(
 				cnt[i] <= {CNT{1'b0}};
 			end
 		end else begin
-			for ( i = 0; i < BTB_D; i = i + 1 ) begin
-				addr_buf[i] <= next_addr_buf[i];
-				tag[i] <= next_tag[i];
-				cnt[i] <= next_cnt[i];
+			if ( buf_we_ ) begin
+				addr_buf[com_idx] <= com_tar_addr;
+				tag[com_idx] <= com_tag;
+			end
+			if ( cnt_we_ ) begin
+				cnt[com_idx] <= next_cnt;
 			end
 		end
 	end
