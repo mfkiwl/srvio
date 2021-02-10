dcd87daa4d3ad6c7bc2745704b4f910a4ea95b8c
ide <ide@ny.ics.keio.ac.jp>
2021-02-10 20:58:29 +0900

    Extended support of vcs

--- cpu/test/clean.sh
+++ cpu/test/clean.sh
@@ -1,8 +1,45 @@
-#!/bin/sh                                                                                                                                                                                     
-rm -f xmverilog.log
-rm -f xmverilog.key
-rm -f xmverilog.history
-rm -rf xcelium.d
-rm -rf waves.shm
-rm -rf .simvision
-rm -rf .bpad
+#!/bin/tcsh
+
+source sim_tool.sh
+
+if ( $SIM_TOOL =~ "xmverilog" ) then
+	# cadence 
+	echo "Removing file xmverilog.log"
+	rm -f xmverilog.log
+
+	echo "Removing file xmverilog.key"
+	rm -f xmverilog.key
+
+	echo "Removing file xmverilog.history"
+	rm -f xmverilog.history
+
+	echo "Removing directory xcelium.d"
+	rm -rf xcelium.d
+
+	echo "Removing directory waves.shm"
+	rm -rf waves.shm
+
+	echo "Removing directory .simvision"
+	rm -rf .simvision
+
+	echo "Removing directory .bpad"
+	rm -rf .bpad
+else if ( $SIM_TOOL =~ "vcs" ) then
+	# synopsys
+	echo "Removing file ucli.key"
+	rm -f ucli.key
+
+	foreach simbin ( *.sim )
+		echo "Removing file $simbin"
+		rm -f $simbin
+	end
+
+	echo "Removing directory csrc"
+	rm -rf ./csrc
+
+	foreach simdir ( *.sim.daidir )
+		echo "Removing directory $simdir"
+		rm -rf $simdir
+	end
+else if ( $SIM_TOOL =~ "verilator" ) then
+endif
